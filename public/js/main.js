40b7e1a3d04c01d43f2a0915fe8e0a4265a9a9d6
Zakaria <zakPro@live.be>
2020-12-28 15:33:56 +0100

    exo-4-part1

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,20 @@
 // prompt 
-let prenom = prompt('ton prenom ?')
-console.log(prenom);
+// let prenom = prompt('ton prenom ?')
+// console.log(prenom);
 
-alert(prenom)
+// alert(prenom)
+
+// EXECRICE 4 PART1
+// let nom = ('Belmaachi')
+// console.log(nom);
+
+// let prenom = ('Zakaria')
+// console.log(prenom);
+
+// let ville = prompt('quelle est votre ville ?')
+// console.log(ville);
+// alert(ville)
+
+// console.log( `${prenom} ${nom}`)
+
 
